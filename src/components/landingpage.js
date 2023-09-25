be7ec5edb3f4bc4464e6dcae4fccbc65e7725775
Vinicius Bueno Costa <buenov535@gmail.com>
2023-09-25 13:02:30 -0300

    componentizada a landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,14 +1,30 @@
 import { createElement } from "../elementBuilder.js";
 
+import { landingpageText } from "../lists/landingpageText.js";
+
 function landingPageImage() {
-    const imageElement = createElement('div', '');
+    const imageElement = createElement('img', 'landingpage-image');
+    imageElement.src = '/assets/minha-foto.jpg';
 
     return imageElement;
 }
 
+function landingPageDescription() {
+    const descriptionContainer = createElement('div', 'landingpage-text');
+    const title = createElement('h2', 'landingpage-title');
+    title.innerText = landingpageText.title;
+    const description = createElement('p', 'landingpage-description');
+    description.innerText = landingpageText.description;
+    descriptionContainer.appendChild(title);
+    descriptionContainer.appendChild(description);
+
+    return descriptionContainer
+}
+
 export const landingPageBuilder = () => {
-    const  landingPageSection = createElement('section', 'landing-page');
+    const  landingPageSection = createElement('section', 'landingpage');
     landingPageSection.appendChild(landingPageImage());
+    landingPageSection.appendChild(landingPageDescription());
 
     return landingPageSection;
 }
---
+++ src/lists/landingpageText.js
@@ -0,0 +1,5 @@
+export const landingpageText = {
+    title: 'Oi! Eu sou Vinícius, um programador full-stack!',
+
+    description: 'Sou um desenvolvedor web, full-stack, já fiz diversos projetos, que você pode conferir por aqui. Comecei meus estudos como autodidata, mas após um tempo preferi seguir um curso pela Trybe para me desenvolver melhor profissionalmente!',
+}
