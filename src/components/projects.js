4b733b203e5bc795efd46f08613a861d3d9278cb
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-04 17:03:02 -0300

    card layout

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,62 +2,91 @@ import { createElement } from "../elementBuilder.js";
 
 import { sectionContent } from "../lists/projectsSection.js";
 
-function cardDescription(item) {
-    const cardContainer = createElement('div', ['card-text-container']);
+function generateTechs(Arr) {
+    const listClasses = ['flex', 'justify-center', 'gap-3'];
+    const techList = createElement('ul', listClasses);
 
-    const cardTitle = createElement('h3', ['card-title']);
-    cardTitle.innerText = item.name;
-
-    const cardDescription = createElement('p', ['card-description']);
-    cardDescription.innerText = item.description;
-
-    [cardTitle, cardDescription].forEach((e) => {
-        cardContainer.appendChild(e);
-    })
-
-    return cardContainer;
-}
-
-function cardimage(item) {
-    const imgContainer = createElement('div', ['info-container']);
-    const techs = createElement('ul', ['card-techs']);
-    item.techs.forEach((tech) => {
-        const tool = createElement('li', ['tech-item']);
-        tool.innerText = tech;
-        techs.appendChild(tool);
-    });
-    const links = createElement('ul', ['card-links']);
-    links.innerHTML = `
-    <li><a href=${item.urlGitHub} target="_blank">GitHub</a></li>
-    <li><a href=${item.urlDeploy} target="_blank">Site</a></li>
-    `;
-    const div = createElement('div', ['hidden-info']);
-    div.appendChild(links);
-    div.appendChild(techs);
-
-    const cardImg = createElement('img', ['card-img']);
-    cardImg.src = item.img;
-
-    [div, cardImg].forEach((e) => {
-        imgContainer.appendChild(e);
+    const techClasses = [];
+    const iconClasses = ['w-6', 'h-6'];
+    const iconColorHex = '8BE9FD';
+    Arr.forEach((tech) => {
+        const newTech = createElement('li', techClasses);
+        const icon = createElement('img', iconClasses);
+        icon.src = `https://cdn.simpleicons.org/${tech}/${iconColorHex}/`;
+        newTech.appendChild(icon);
+        techList.appendChild(newTech);
     });
 
-    return imgContainer;
+    return techList;
 }
 
-function toggleHover(e) {
-    e.target.classList.toggle('hover');
+function generateLinks(links) {
+    const listClasses = ['flex', 'justify-center', 'gap-5'];
+    const list = createElement('div', listClasses);
+
+    const linkClasses = [];
+    const iconClasses = ['w-6', 'h-6'];
+    links.forEach((l) => {
+        const newLink = createElement('a', linkClasses);
+        newLink.href = l.url;
+        newLink.target = '_blank';
+
+        const icon = createElement('img', iconClasses);
+        icon.src = l.icon;
+
+        newLink.appendChild(icon);
+        list.appendChild(newLink);
+    });
+
+    return list;
+}
+
+function cardDescription(txt) {
+    const textContainerClasses = ['flex', 'flex-col', 'w-11/12', 'self-center', 'justify-center', 'align-start', 'h-32', 'overflow-scroll'];
+    const textContainer = createElement('div', textContainerClasses);
+
+    const titleClasses = ['text-cyan', 'font-semibold', 'text-xl', 'self-center'];
+
+    txt.forEach((paragraph) => {
+        const classes = paragraph.isTitle ? titleClasses : [];
+        const newParagraph = createElement(paragraph.tag, classes);
+
+        textContainer.appendChild(newParagraph);
+    });
+
+    return textContainer;
+}
+
+function cardimage(img) {
+    const containerClasses = ['relative', 'text-cyan']
+    const container = createElement('div', containerClasses);
+
+    const hiddenContainerClasses = ['absolute', 'left-0', 'right-0', 'p-2', 'flex', 'flex-col', 'justify-between', 'h-full', 'bg-background', 'bg-opacity-75', 'border-b', 'border-cyan'];
+    const hiddenContainer = createElement('div', hiddenContainerClasses);
+
+    hiddenContainer.appendChild(generateLinks(img.links));
+    hiddenContainer.appendChild(generateTechs(img.techs));
+
+    const cardImg = createElement('img', []);
+    cardImg.src = img.imgUrl;
+
+    [hiddenContainer, cardImg].forEach((newElement) => {
+        container.appendChild(newElement);
+    });
+
+    return container;
 }
 
 function loadProjects() {
-    const projectList = createElement('ul', ['project-list']);
+    const listClasses = ['flex', 'flex-col', 'md:flex-row', 'md:flex-wrap', 'gap-2', 'md:justify-between'];
+    const projectList = createElement('ul', listClasses);
+
+    const cardClasses = ['flex', 'flex-col', 'border', 'rounded-xl', 'p-2', 'md:w-64', 'md:gap-0', 'md:p-3'];
 
     sectionContent.forEach((project) => {
-        const card = createElement('li', ['project-card']);
-        card.appendChild(cardimage(project));
-        card.appendChild(cardDescription(project));
-        card.addEventListener('mouseenter', toggleHover);
-        card.addEventListener('mouseleave', toggleHover);
+        const card = createElement('li', cardClasses);
+        card.appendChild(cardimage(project.img));
+        card.appendChild(cardDescription(project.text));
 
         projectList.appendChild(card);
     });
@@ -66,10 +95,12 @@ function loadProjects() {
 }
 
 export const projectsElement = () => {
-    const projectsSection = createElement('section', ['container']);
+    const sectionClasses = ['container', 'max-w-3xl', 'self-center', 'pt-10', 'px-2', 'md:px-0'];
+    const projectsSection = createElement('section', sectionClasses);
     projectsSection.id = 'projects';
 
-    const title = createElement('h2', ['content-title']);
+    const titleClasses = ['text-cyan', 'font-bold', 'text-2xl'];
+    const title = createElement('h2', titleClasses);
     title.innerText = 'Projetos.';
 
     projectsSection.appendChild(title);
--- src/lists/projectsSection.js
+++ src/lists/projectsSection.js
@@ -1,50 +1,147 @@
 export const sectionContent = [
     {
-        name: 'To-Do List',
-        urlGitHub: 'https://github.com/Buenno01/todo-list',
-        urlDeploy: 'https://buenno01.github.io/todo-list/',
-        img: '/assets/todo-list.png',
-        description: 'Esse projeto utiliza apenas a minupulação do DOM com javaScript para a construção do HTML da página, e conta com salvamento em local storage das tarefas criadas e concluídas.',
-        techs: [
-            'CSS',
-            'JS',
-            'HTML',
+        text: [
+            {
+                tag: 'h4',
+                content: 'To-Do List',
+                isTitle: true,
+            },
+            {
+                tag: 'p',
+                content: 'Esse projeto utiliza apenas a minupulação do DOM com javaScript para a construção do HTML da página, e conta com salvamento em local storage das tarefas criadas e concluídas.',
+            }
         ],
+        img: {
+            imgUrl: '/assets/todo-list.png',
+            links: [
+                {
+                    icon: 'https://cdn.simpleicons.org/github/8BE9FD/',
+                    url: 'https://github.com/Buenno01/todo-list',
+                },
+                {
+                    icon: '/assets/icons/rocket.svg',
+                    url: 'https://buenno01.github.io/todo-list/',
+                },
+            ],
+            techs: 
+            [
+                'css3',
+                'javascript',
+                'html5',
+            ],
+        }
+        
     },
     {
-        name: 'Weather',
-        urlGitHub: 'https://github.com/Buenno01/weather',
-        urlDeploy: 'https://weather-mhka0e2zi-buenno01.vercel.app/',
-        img: '/assets/weather.png',
-        description: 'Esse projeto consome uma API de previsão do tempo, retornando informações em tempo real do estado de temperatura, chuva e umidade do local pesquisado.',
-        techs: [
-            'CSS',
-            'JS',
-            'HTML',
+        text: [
+            {
+                tag: 'h4',
+                content: 'Weather',
+                isTitle: true,
+            },
+            {
+                tag: 'p',
+                content: 'Esse projeto consome uma API de previsão do tempo, retornando informações em tempo real do estado de temperatura, chuva e umidade do local pesquisado.',
+            }
         ],
+        img: {
+            imgUrl: '/assets/weather.png',
+            links: [
+                {
+                    icon: 'https://cdn.simpleicons.org/github/8BE9FD/',
+                    url: 'https://github.com/Buenno01/weather',
+                },
+                {
+                    icon: '/assets/icon/rocket.svg',
+                    url: 'https://weather-mhka0e2zi-buenno01.vercel.app/',
+                },
+            ],
+            techs: 
+            [
+                'css3',
+                'javascript',
+                'html5',
+            ],
+        }
+        
     },
     {
-        name: 'Calculator',
-        urlGitHub: 'https://github.com/Buenno01/Calculator',
-        urlDeploy: 'https://buenno01.github.io/Calculator/',
-        img: '/assets/calculator.png',
-        description: 'Uma simples calculadora que mostra em tempo real os cálculos.',
-        techs: [
-            'CSS',
-            'JS',
-            'HTML',
+        text: [
+            {
+                tag: 'h4',
+                content: 'Calculator',
+                isTitle: true,
+            },
+            {
+                tag: 'p',
+                content: 'Uma calculadora que mostra em tempo real os cálculos.',
+            }
         ],
+        img: {
+            imgUrl: '/assets/calculator.png',
+            links: [
+                {
+                    icon: 'https://cdn.simpleicons.org/github/8BE9FD/',
+                    url: 'https://github.com/Buenno01/Calculator',
+                },
+                {
+                    icon: '/assets/icon/rocket.svg',
+                    url: 'https://buenno01.github.io/Calculator/',
+                },
+            ],
+            techs: 
+            [
+                'css3',
+                'javascript',
+                'html5',
+            ],
+        }
+        
     },
     {
-        name: 'pixel-editor',
-        urlGitHub: 'https://github.com/Buenno01/pixel-editor',
-        urlDeploy: 'https://buenno01.github.io/pixel-editor/',
-        img: '/assets/pixel-editor.png',
-        description: 'Essa aplicação te permite selecionar cores e colorir os pixels de um grid, é um exercício de manipulação do dom e do storage.',
-        techs: [
-            'CSS',
-            'JS',
-            'HTML',
+        text: [
+            {
+                tag: 'h4',
+                content: 'Pixel Editor',
+                isTitle: true,
+            },
+            {
+                tag: 'p',
+                content: 'Essa aplicação te permite selecionar cores e colorir os pixels de um grid, é um exercício de manipulação do dom e do storage.',
+            }
         ],
+        img: {
+            imgUrl: '/assets/pixel-editor.png',
+            links: [
+                {
+                    icon: 'https://cdn.simpleicons.org/github/8BE9FD/',
+                    url: 'https://github.com/Buenno01/pixel-editor',
+                },
+                {
+                    icon: '/assets/icon/rocket.svg',
+                    url: 'https://buenno01.github.io/pixel-editor/',
+                },
+            ],
+            techs: 
+            [
+                'css3',
+                'javascript',
+                'html5',
+            ],
+        }
+        
     },
 ]
+
+const techUrl = {
+
+    tailwind: 'https://cdn.simpleicons.org/tailwindcss/06B6D4/',
+
+    vue3: 'https://cdn.simpleicons.org/vuedotjs/4FC08D/',
+
+    Github: 'https://cdn.simpleicons.org/github/8BE9FD/',
+
+    Git: 'https://cdn.simpleicons.org/git/F05032/',
+
+    React: 'https://cdn.simpleicons.org/react/61DAFB/',
+}
