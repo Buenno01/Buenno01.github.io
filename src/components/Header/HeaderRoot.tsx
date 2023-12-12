6073c2e635d1a0b9e9b9933bb0c73ee7e60a6423
Vinicius Bueno Costa <buenov535@gmail.com>
2023-12-12 10:52:25 -0300

    fixed header bg opacity on dark mode

--- src/components/Header/HeaderRoot.tsx
+++ src/components/Header/HeaderRoot.tsx
@@ -11,7 +11,7 @@ function HeaderRoot({ children }:HeaderRootProps) {
       <header
         className={ `flex justify-between items-center pr-2 w-full 
         fixed max-h-9 z-10 text-lg md:container md:self-center 
-        bg-white dark:bg-zinc-800 bg-opacity-60 md:p-0` }
+        bg-white dark:bg-zinc-800 dark:bg-opacity-60 bg-opacity-60 md:p-0` }
       >
         { children }
 
