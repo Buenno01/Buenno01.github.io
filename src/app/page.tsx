eb341b8cefef7d6477ca4c75f00073c71b184df7
Vinícius Bueno Costa <buenov535@gmail.com>
2025-01-21 20:06:44 -0300

    feat: adjusted article card grids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>    
       <LocomotiveScroll></LocomotiveScroll>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-11/12 mx-auto my-36">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-11/12 mx-auto my-36">
         {
           projects.map((project, index) => (
             <ArticleCard key={ index } { ...project } />
