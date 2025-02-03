fb15fcc4e1ca6a2ddad97d197509c85031bb80dd
Vinícius Bueno Costa <buenov535@gmail.com>
2025-02-03 19:50:08 -0300

    feat: added new about data

---
+++ src/data/about.ts
@@ -0,0 +1,23 @@
+const about = {
+  name: 'Vinícius Bueno Costa',
+  role: 'Fullstack Developer | Shopify Developer',
+  description: 'I am a Fullstack Developer with a passion for creating and building new things. I have experience with a wide range of technologies, including React, Next.js, Node.js, and Shopify. I am always looking for new opportunities to learn and grow as a developer.',
+  socials: [
+    {
+      iconKey: 'github',
+      name: 'GitHub',
+      url: 'https://github.com/Buenno01',
+      backgroundColor: '#fff',
+      textColor: '#000',
+    },
+    {
+      iconKey: 'linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/viniciusbuenocosta/',
+      backgroundColor: '#0e76a8',
+      textColor: '#fff',
+    }
+  ]
+};
+
+export default about;
