ae0007daa33078a38647ac8b5b93f3011057adeb
Vinícius Bueno Costa <buenov535@gmail.com>
2025-02-05 19:51:55 -0300

    fix: removed old code from directory

--- old-react/assets/projectImages.ts
+++
@@ -1,4 +0,0 @@
-export { default as CalculatorProj } from './calculator.png';
-export { default as PixelProj } from './pixel-editor.png';
-export { default as ToDoProj } from './todo-list.png';
-export { default as WeatherProj } from './weather.png';
